8344620f819dd46a09704c5a9ac2c6defa337c7f
liwei <eiri@fsi.co.jp>
2018-07-26 10:13:00 +0900

    to add generated entities

--- src/main/java/org/jhipster/blog/config/CacheConfiguration.java
+++ src/main/java/org/jhipster/blog/config/CacheConfiguration.java
@@ -38,6 +38,18 @@ public class CacheConfiguration {
             cm.createCache(org.jhipster.blog.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(org.jhipster.blog.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(org.jhipster.blog.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Region.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Country.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Location.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Department.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Department.class.getName() + ".employees", jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Task.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Task.class.getName() + ".jobs", jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Employee.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Employee.class.getName() + ".jobs", jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Job.class.getName(), jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.Job.class.getName() + ".tasks", jcacheConfiguration);
+            cm.createCache(org.jhipster.blog.domain.JobHistory.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
